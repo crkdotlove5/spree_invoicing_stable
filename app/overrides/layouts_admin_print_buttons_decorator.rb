8d93983e7541604a1614580766194db4ef00f5d0
Sai Kiran <sai.kiran@partheas.com>
2013-08-07 13:58:39 +0530

    Updated CODE v2

--- app/overrides/layouts_admin_print_buttons_decorator.rb
+++ app/overrides/layouts_admin_print_buttons_decorator.rb
@@ -34,3 +34,29 @@
                        :remove => '#footer[data-hook]',
                        :original => '410a8316104f80d956fccfe598058832182e7a91'
   )
+
+  ###JIRAFE
+
+
+  # Remove Jirafe's javascript script code from header of all pages
+  Deface::Override.new(:virtual_path => Spree::Config[:layout],
+                       :name => "remove_analytics_header",
+                       :insert_before => "a.icon-dashboard",
+                       :text => "<h1>HI</h1>")
+
+# Remove Jirafe's sidebar link from admin interface
+  Deface::Override.new(:virtual_path =>
+                           "spree/admin/shared/_configuration_menu",
+                       :name => "remove_dashboard_sidebar_link",
+                       :remove => "code[erb-loud]:contains('jirafe')")
+
+# Remove Jirafe's content for head in admin interface
+  Deface::Override.new(:virtual_path => "spree/admin/overview/index",
+                       :name => "remove_head_content",
+                       :remove => "code[erb-silent]:contains('content_for
+:head')",
+                       :closing_selector => "code[erb-silent]:contains('end')")
+  Deface::Override.new(:virtual_path => "spree/layouts/admin",
+                       :name => "add_tenant_header_link",
+                       :insert_bottom => "ul[data-link=admin_login_navigation_bar]",
+                       :text => "<%= configurations_sidebar_menu_item t(:jirafe), admin_analytics_path %>")
